f9e54e0feccc629ab365590b73e144dcd3cf84d0
Connor Hansen <chansen13@georgefox.edu>
2021-08-13 10:12:19 -0700

    add custom questions, basic formatting and functionality. Need to add logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,206 @@
 $(document).ready(function () {
   var javaScript = 0;
   var python = 0;
-  var basic = 0;
+  var cSharp = 0;
   var sql = 0;
 
   $("#formRadio1").submit(function (event) {
     event.preventDefault();
+    // add results hide for reset - note taken at 9:10 am
     $("#formRadio2,#formRadio3,#formRadio4,#formRadio5,#formRadio6,#formRadio7").hide();
-    const celeb = $("input:radio[name=answer1]:checked").val();
-    if (celeb === "kermit") {
+    const activity = $("input:radio[name=answer1]:checked").val();
+    if (activity === "javaScript") {
       $("#formRadio1").hide();
       $("#formRadio2").show();
-      return kermit++;
-    } else if (celeb === "mspiggy") {
+      return javaScript++;
+    } else if (activity === "python") {
       $("#formRadio1").hide();
       $("#formRadio2").show();
-      return mspiggy++;
-    } else if (celeb === "chef") {
+      return python++;
+    } else if (activity === "cSharp") {
       $("#formRadio1").hide();
       $("#formRadio2").show();
-      return chef++;
+      return cSharp++;
+    } else if (activity === "sql") {
+      $("#formRadio1").hide();
+      $("#formRadio2").show();
+      return sql++;
     } else {
     }
   });
 
   $("#formRadio2").submit(function (event) {
     event.preventDefault();
-    const food = $("input:radio[name=answer2]:checked").val();
-    if (food === "kermit") {
+    const fear = $("input:radio[name=answer2]:checked").val();
+    if (fear === "javaScript") {
       $("#formRadio2").hide();
       $("#formRadio3").show();
-      return kermit++;
-    } else if (food === "mspiggy") {
+      return javaScript++;
+    } else if (fear === "python") {
       $("#formRadio2").hide();
       $("#formRadio3").show();
-      return mspiggy++;
-    } else if (food === "chef") {
+      return python++;
+    } else if (fear === "cSharp") {
       $("#formRadio2").hide();
       $("#formRadio3").show();
-      return chef++;
+      return cSharp++;
+    } else if (fear === "sql") {
+      $("#formRadio2").hide();
+      $("#formRadio3").show();
+      return sql++;
+    } else {
+    }
+  });
+
+  $("#formRadio3").submit(function (event) {
+    event.preventDefault();
+    const relax = $("input:radio[name=answer3]:checked").val();
+    if (relax === "javaScript") {
+      $("#formRadio3").hide();
+      $("#formRadio4").show();
+      return javaScript++;
+    } else if (relax === "python") {
+      $("#formRadio3").hide();
+      $("#formRadio4").show();
+      return python++;
+    } else if (relax === "cSharp") {
+      $("#formRadio3").hide();
+      $("#formRadio4").show();
+      return cSharp++;
+    } else if (relax === "sql") {
+      $("#formRadio3").hide();
+      $("#formRadio4").show();
+      return sql++;
+    } else {
+    }
+  });
+
+  $("#formRadio4").submit(function (event) {
+    event.preventDefault();
+    const lang = $("input:radio[name=answer4]:checked").val();
+    if (lang === "javaScript") {
+      $("#formRadio4").hide();
+      $("#formRadio5").show();
+      return javaScript++;
+    } else if (lang === "python") {
+      $("#formRadio4").hide();
+      $("#formRadio5").show();
+      return python++;
+    } else if (lang === "cSharp") {
+      $("#formRadio4").hide();
+      $("#formRadio5").show();
+      return cSharp++;
+    } else if (lang === "sql") {
+      $("#formRadio4").hide();
+      $("#formRadio5").show();
+      return sql++;
+    } else {
+    }
+  });
+
+  $("#formRadio5").submit(function (event) {
+    event.preventDefault();
+    const dnd = $("input:radio[name=answer5]:checked").val();
+    if (dnd === "javaScript") {
+      $("#formRadio5").hide();
+      $("#formRadio6").show();
+      return javaScript++;
+    } else if (dnd === "python") {
+      $("#formRadio5").hide();
+      $("#formRadio6").show();
+      return python++;
+    } else if (dnd === "cSharp") {
+      $("#formRadio5").hide();
+      $("#formRadio6").show();
+      return cSharp++;
+    } else if (dnd === "sql") {
+      $("#formRadio5").hide();
+      $("#formRadio6").show();
+      return sql++;
+    } else {
+    }
+  });
+
+  $("#formRadio6").submit(function (event) {
+    event.preventDefault();
+    const hangOut = $("input:radio[name=answer6]:checked").val();
+    if (hangOut === "javaScript") {
+      $("#formRadio6").hide();
+      $("#formRadio7").show();
+      return javaScript++;
+    } else if (hangOut === "python") {
+      $("#formRadio6").hide();
+      $("#formRadio7").show();
+      return python++;
+    } else if (hangOut === "cSharp") {
+      $("#formRadio6").hide();
+      $("#formRadio7").show();
+      return cSharp++;
+    } else if (hangOut === "sql") {
+      $("#formRadio6").hide();
+      $("#formRadio7").show();
+      return sql++;
     } else {
     }
   });
 
 
-  $("#formRadio3").submit(function (event) {
-    event.preventDefault();
-    const home = $("input:radio[name=answer3]:checked").val();
-    if (home === "kermit") {
-      kermit++;
-      $("#formRadio3").hide();
-      // alert(kermit);
-      // alert(mspiggy);
-      // alert(chef);
 
-    } else if (home === "mspiggy") {
-      mspiggy++;
-      $("#formRadio3").hide();
-      // alert(kermit);
-      // alert(mspiggy);
-      // alert(chef);
-    } else if (home === "chef") {
-      chef++;
-      $("#formRadio3").hide();
+
+
+  $("#formRadio7").submit(function (event) {
+    event.preventDefault();
+    const music = $("input:radio[name=answer7]:checked").val();
+    if (music === "javaScript") {
+      javaScript++;
+      $("#formRadio6").hide();
+      // alert(javaScript);
+      // alert(python);
+      // alert(cSharp);
+      // alert(sql);
+
+    } else if (music === "python") {
+      python++;
+      $("#formRadio6").hide();
+      // alert(javaScript);
+      // alert(python);
+      // alert(cSharp);
+      // alert(sql);
+    } else if (music === "cSharp") {
+      cSharp++;
+      $("#formRadio6").hide();
       // Test whether scores are being tabulated
-      // alert(kermit);
-      // alert(mspiggy);
-      // alert(chef);   
+      // alert(javaScript);
+      // alert(python);
+      // alert(cSharp);   
+      // alert(sql);   
+    } else if (music === "sql") {
+      sql++;
+      $("#formRadio6").hide();
+      // Test whether scores are being tabulated
+      // alert(javaScript);
+      // alert(python);
+      // alert(cSharp);   
+      // alert(sql);   
     } else {
     }
+
     // Calculate and show Results
-    $("#showResult").show();
-    if (kermit >= 2) {
+    $("#showResult,#btnReset").show();
+    if (javaScript >= 4) {
       // alert("testfrog");
-      $("#kermitResult").show();
-    } else if (mspiggy >= 2) {
+      $("#javaScriptResult").show();
+    } else if (python >= 4) {
       // alert("testpig");
-      $("#mspiggyResult").show();
-    } else if (chef >= 2) {
-      // alert("testchef");
-      $("#chefResult").show();
+      $("#pythonResult").show();
+    } else if (cSharp >= 4) {
+      // alert("testcSharp");
+      $("#cSharpResult").show();
+    } else if (sql >= 4) {
+      // alert("testcSharp");
+      $("#cSharpResult").show();
     } else {
+      // if (javaScript ===)
       // $("#showResult").hide();
       $("#noResult").show()
       // alert("testno");
